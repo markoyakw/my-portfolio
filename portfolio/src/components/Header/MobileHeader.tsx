e9c1160cc47d331a6b38a05e751d768e6270b5e0
mrkykvnk <yakovenkomarko@gmail.com>
2024-12-25 23:58:45 +0000

    Added mobile header

--- portfolio/src/components/Header/MobileHeader.tsx
+++ portfolio/src/components/Header/MobileHeader.tsx
@@ -1,8 +1,46 @@
+import { TiThMenu } from "react-icons/ti"
 import classes from "./Header.module.css"
+import { FaChevronUp } from "react-icons/fa6"
+import { AiFillMessage } from "react-icons/ai"
+import { useEffect, useState } from "react"
 
 const MobileHeader = () => {
+
+    const [scrollTopState, setScrollTopState] = useState(0)
+    const [isMobileHeaderVisible, setIsMobileHeaderVisible] = useState(true)
+
+    useEffect(() => {
+        document.addEventListener("scroll", () => {
+            const scrollTop = document.documentElement.scrollTop
+            setScrollTopState(oldValue => {
+                console.log(oldValue, scrollTop)
+                if (oldValue > scrollTopState) {
+                    setIsMobileHeaderVisible(false)
+                }
+                else {
+                    setIsMobileHeaderVisible(true)
+                }
+                return scrollTop
+            })
+        })
+    })
+
+    const mobileHeaderClassName = `${classes["mobile-header"]} ${isMobileHeaderVisible ? classes["mobile-header--visible"] : ""}`
+
     return (
-        <div>MobileHeader</div>
+        <div className={mobileHeaderClassName}>
+            <div className={classes["mobile-header__main-row"]}>
+                <button className={classes["mobile-header__button"]} >
+                    <TiThMenu size={"70%"} />
+                </button>
+                <button className={classes["mobile-header__button"]} >
+                    <AiFillMessage size={"70%"} />
+                </button>
+            </div>
+            <button className={classes["mobile-header__button"]} >
+                <FaChevronUp size={"70%"} />
+            </button>
+        </div>
     )
 }
 
--- portfolio/src/components/MainLayout/MainLayout.tsx
+++ portfolio/src/components/MainLayout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react"
 import Header from "@components/Header/Header"
 import classes from "./MainLayout.module.css"
+import MobileHeader from "@components/Header/MobileHeader"
 
 type TMainLayoutProps = {
   children: ReactNode
@@ -10,6 +11,7 @@ const MainLayout: FC<TMainLayoutProps> = ({ children }) => {
   return (
     <div className={classes["layout"]}>
       <Header />
+      <MobileHeader />
       <div className={classes["layout__body"]}>
         {children}
       </div>
